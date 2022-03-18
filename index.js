b1cbfe24201792194b2dd6376332383e3bf75751
Ryan Carrion <ryan.carrion.pro@gmail.com>
2022-03-18 02:00:25 +0100

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const { CommandoClient, Client } = require('discord.js-commando');
 const path = require('path');
-const {prefix, token} = require('./config.json');
 
 const client = new CommandoClient({
     commandPrefix: '-',
     owner: '352491753544351747',
     invite: 'https://discord.gg/fhYj9ahn', 
-    invite: 'https://discord.gg/YE2T2qNG'
+    invite: 'https://discord.gg/YE2T2qNG',
+    invite: 'https://discord.gg/5xBkCBYu',
+    invite: 'https://discord.gg/AhgFZffp'
 });
 
 client.registry
@@ -30,4 +31,4 @@ client.once('ready', () => {
 
 client.on('error', (error) => console.error(error));
 
-client.login(process.env.TOKEN);
+client.login('OTUzNzUzNjkyODA3NTI0NDUz.YjJKWA.lXrsOQxNbs94u-MN2RECYHqd9GA');
